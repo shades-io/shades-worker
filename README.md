ee1f363c2855e0ca22bbf8bd936608dd253b7f4d
Alexander Biskop <abiskop@netflix.com>
2017-06-09 12:30:42 -0700

    Set up initial scaffolding for parsing command line args

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # shades-worker
 Worker for Shades
+
+
+### Implementation TODOs
+
+- Process startup, add a way to configure which persistence module(s) and which projection modules to use. 
+  - For now, we statically install persistence and projection modules as dependencies; can figure out a way to do that dynamically later.
+  - Configure which modules to use during startup
+- Listen to worker queue using shades-xyz module
+- On event, fetch current projection state
+  - Pass into each projection module, together with the event
+- Publish projection diff to pub-sub topic using shades-xyz module
+- 
+
+
+### Notes
+
+#### TODO Example for starting the worker
+
+```
+node index.js --store redis --projection graphlib --projection bar --shades-store-redis:host='10.127.0.1'
+```
---
+++ index.js
@@ -0,0 +1,79 @@
+/*
+ * Worker process entry point.
+ */
+
+'use strict';
+
+const argv = require('minimist')(process.argv.slice(), {
+    string: ['store'],
+    alias: {
+        'store': 's'
+    }
+});
+const log = require('bunyan').createLogger({
+    name: 'shades-worker',
+    level: 'debug'
+});
+/* Match namespaced, module-specific args, e.g. "shades-store-redis:hostname",
+   capturing the module name and the key of the argument */
+const namespacedArgsRegex = /^(shades-.+):(.+)$/;
+
+const createWorker = require('./lib/worker');
+
+const start = () => {
+    log.debug('argv', argv);
+    const options = parseArgs();
+    log.info('Starting with options', options);
+
+    const worker = createWorker({
+        store: importModule(
+            options.storeModuleName,
+            options.moduleOptions[options.storeModuleName]),
+        projections: options.projectionModuleNames.map(name => importModule(
+            name,
+            options.moduleOptions[name]))
+    });
+};
+
+const parseArgs = () => {
+    validateArgs();
+    const projectionArgs = [].concat(argv.projection);
+    const projectionModuleNames = projectionArgs
+        .map(projection => `shades-projection-${projection}`);
+
+    const moduleOptions = Object
+        .keys(argv)
+        .filter(key => namespacedArgsRegex.test(key))
+        .reduce((aggr, key) => {
+            const [_, moduleName, optionKey] = namespacedArgsRegex.exec(key);
+            aggr[moduleName] = Object.assign({}, aggr[moduleName], {
+                [optionKey]: argv[key]
+            });
+            return aggr;
+        }, {});
+
+    return {
+        storeModuleName: `shades-store-${argv.store}`,
+        projectionModuleNames,
+        moduleOptions
+    };
+};
+
+const validateArgs = () => {
+    const requiredKeys = ['store', 'projection'];
+    requiredKeys.forEach(key => {
+        if (argv[key] === undefined) {
+            throw new Error(`Argument "${key}" is required.`);
+        }
+    });
+};
+
+const importModule = (name, options) => {
+    log.debug(`Importing module ${name}`);
+    /* TODO temp hack: mock "require" */
+    const require = () => (() => {});
+
+    return require(name)(options);
+}
+
+start();
